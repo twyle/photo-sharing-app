53102adc093bd88b86e755d4394dee9ca694a046
twyle <lyceokoth@gmail.com>
2023-03-02 13:07:49 +0300

    creates the feature development workflow

--- services/app/api/__init__.py
+++ services/app/api/__init__.py
@@ -1,11 +1,14 @@
 from flask import Flask
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 def create_app() -> Flask:
     """Create the Flask App instance."""
     app = Flask(__name__)
     
-    @app.route('/')
+    @app.route('/health')
     def home():
         return 'Hello world!'
     
